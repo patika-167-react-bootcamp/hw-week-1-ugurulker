b481989d05a88033219ebe65c9b16908df66d4e5
ugurulker <ugurulkerr@outlook.com>
2022-02-10 18:15:20 +0300

    Arrangements were made

--- hw-2.js
+++ hw-2.js
@@ -1,11 +1,12 @@
-Array.prototype.includesCi = function(name) {
-    let String = name.toLowerCase(); // Girilen değer küçük harfe çevirildi.
-    for (let i = 0; i < this.length; i++) {
-      this[i] = this[i].toLowerCase(); // Array içindeki değerler küçük harfe çevirildi.
-      if(this[i] == String){
-          return true; // Girilen değer ve Array içindeki değer karşılaştırıldı.
-      } else {
-          return false;
-      }
-    };
+Array.prototype.includesCi = function (name) {
+  let String = name.toLowerCase(); // Girilen değer küçük harfe çevirildi.
+  let array = []; // Orjinal Array değişmesin istediğimiz için yeni bir array oluşturduk.
+  for (let i = 0; i < this.length; i++) {
+    array[i] = this[i].toLowerCase(); // Array içindeki değerler küçük harfe çevirildi.
+    if (array[i] == String) {
+      return true; // Girilen değer ve Array içindeki değer karşılaştırıldı.
+    } else {
+      return false;
+    }
+  }
 };
